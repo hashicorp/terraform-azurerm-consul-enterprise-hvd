7f98ff0068216d36447f8313927fd80672ad4991
Adam Buxton <buxton.adam@gmail.com>
2024-10-22 15:07:57 +0100

    syntax

--- examples/ubuntu-nlb-consul-primary/variables.tf
+++ examples/ubuntu-nlb-consul-primary/variables.tf
@@ -130,7 +130,7 @@ variable "consul_config_template" {
   description = "(Optional string) name of `*.tpl` file in the `./templates` folder local to the module decleration, to replace the root `server.hcl.tpl` "
   validation {
     condition     = var.consul_config_template == null || can(fileexists("${path.cwd}/templates/${var.consul_config_template}"))
-    error_message = "File `${path.cwd}/templates/${var.consul_config_template}` not found or not readable"
+    error_message = "File not found or not readable"
   }
 }
 variable "consul_vm_size" {
